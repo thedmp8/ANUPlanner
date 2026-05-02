6f1e09d20aae101812af1147757b19c000ee2bca
Dylan <thedmp8@gmail.com>
2026-05-02 17:49:27 +1000

    Added automatic timeline calcs

--- Models/DegreeRequirement.cs
+++ Models/DegreeRequirement.cs
@@ -2,8 +2,12 @@ namespace ANUPlanner.Models;
 
 public class DegreeRequirement
 {
+    private const int UnitsPerAcademicYear = 48;
+
     public string DegreeId { get; set; } = "";
     public string DegreeName { get; set; } = "";
     public int RequiredUnits { get; set; }
     public List<string> CoreCourses { get; set; } = new();
+
+    public int EstimatedYears => Math.Max(1, (int)Math.Ceiling(RequiredUnits / (double)UnitsPerAcademicYear));
 }
